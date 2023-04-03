612b47bc4267ec8e6acf9aa7944c31a18ea8eab9
Asharamart2075@gmail.com <Asharamart2075@gmail.com>
2023-04-02 21:40:55 -0500

    Added more license links

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -15,16 +15,19 @@ function renderLicenseLink(license) {
 
   // Map the license type to its corresponding URL
   const licenseUrls = {
-    'MIT': 'https://opensource.org/licenses/MIT',
-    'GNU GPL v3': 'https://www.gnu.org/licenses/gpl-3.0',
-    'Apache 2.0': 'https://opensource.org/licenses/Apache-2.0',
-    'GNU AGPLv3':
-    'GNU GPLv3':
-    'GNU LGPLv3':
-    'Mozilla Public License 2.0':
-    'Boost Software License 1.0':
-    'The Unlicense':
-    // Add more licenses as needed
+'MIT': 'https://opensource.org/licenses/MIT',
+'GNU GPL v3':'https://www.gnu.org/licenses/gpl-3.0', 
+'Apache 2.0':'https://opensource.org/licenses/Apache-2.0',
+
+
+'GNU AGPLv3':'https://choosealicense.com/licenses/agpl-3.0/',
+'GNU GPLv3':'https://choosealicense.com/licenses/gpl-3.0/',
+
+'GNU LGPLv3': 'https://choosealicense.com/licenses/lgpl-3.0/',    
+'Mozilla Public License 2.0':'https://choosealicense.com/licenses/mpl-2.0/',
+'Boost Software License 1.0':'https://choosealicense.com/licenses/bsl-1.0/',
+'The Unlicense':'https://choosealicense.com/licenses/unlicense/',
+    //Added all findable licenses
   };
 
   return `[${license}](${licenseUrls[license]})`;
@@ -34,15 +37,23 @@ function renderLicenseLink(license) {
 function renderLicenseSection(license) {
   if (!licenseOption === 'MIT License') {
     licenseOption = 'MITLicense';
-    licenseOption = 'https://choosealicense.com/licenses/mit/';
+    licenseOption = 'https://choosealicense.com/licenses/mit/'; //License 1 
   }
   if (!licenseOption === 'GNU GPL v3') {
     licenseOption = 'GNU GPL v3';
-    licenseOption = 'https://www.gnu.org/licenses/gpl-3.0';
+    licenseOption = 'https://www.gnu.org/licenses/gpl-3.0'; //License 2 
   }
   if (!licenseOption === 'Apache 2.0') {
     licenseOption = 'Apache 2.0';
-    licenseOption = 'https://opensource.org/licenses/Apache-2.0';
+    licenseOption = 'https://opensource.org/licenses/Apache-2.0'; //License 3
+  }
+  if (!licenseOption === 'GNU AGPLv3') {
+    licenseOption = 'GNU AGPLv3';
+    licenseOption = 'GNU AGPLv3':'https://choosealicense.com/licenses/agpl-3.0/'; //License 4 
+  }
+  if (!licenseOption === 'GNU GPLv3') {
+    licenseOption = 'GNU GPLv3';
+    licenseOption = 'https://choosealicense.com/licenses/gpl-3.0/'; //License 5
   }
   return `
 ## License
